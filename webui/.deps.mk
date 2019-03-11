2947ae68a624e34b1d73853b18beea8475b84b3f
Nicolas Pavie <nicolas.pavie@braillenet.org>
2019-03-11 11:10:51 +0100

    Modification de dépendance de xproc-engine-calabash dans le pom de xprocspec

---
+++ webui/.deps.mk
@@ -0,0 +1,4 @@
+.SECONDARY : webui/.dependencies
+webui/.dependencies : \
+	$(MVN_WORKSPACE)/org/daisy/pipeline/clientlib-java/5.0.0-SNAPSHOT/clientlib-java-5.0.0-SNAPSHOT.jar \
+	$(MVN_WORKSPACE)/org/daisy/pipeline/clientlib-java-httpclient/2.1.0-SNAPSHOT/clientlib-java-httpclient-2.1.0-SNAPSHOT.jar
