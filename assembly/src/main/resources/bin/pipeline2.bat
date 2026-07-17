dc20bedc011880a38a891243032865ee706aac7d
Nicolas PAVIE <n.pavie@avh.asso.fr>
2026-07-17 14:55:47 +0200

    dev: update launcher and don't stop conversion on unsupported script options
    This is to still convert if an argument that is not supported by the script is passed to the command line (like some footnotes options)

--- assembly/src/main/resources/bin/pipeline2.bat
+++ assembly/src/main/resources/bin/pipeline2.bat
@@ -55,7 +55,8 @@ goto :EOF
 rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 :BEGIN
-    call:warn %DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
+    call:warn %DATE% %TIME%
+    REM call:warn %DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2% %TIME:~0,2%:%TIME:~3,2%:%TIME:~6,2%
 
     rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -87,8 +88,9 @@ rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
         )
     )
 
-    set DEFAULT_JAVA_OPTS=-Dcom.sun.management.jmxremote
-    if "%JAVA_OPTS%" == "" set JAVA_OPTS=%DEFAULT_JAVA_OPTS%
+    REM NP - disabling jmxremote by default to avoid a firewall notice
+    REM set DEFAULT_JAVA_OPTS=-Dcom.sun.management.jmxremote
+    REM if "%JAVA_OPTS%" == "" set JAVA_OPTS=%DEFAULT_JAVA_OPTS%
 
     set DEFAULT_JAVA_DEBUG_OPTS=-Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005
     if "%PIPELINE2_DEBUG%" == "" goto :PIPELINE2_DEBUG_END
@@ -104,6 +106,9 @@ rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     set MODE=webservice
 :RUN_LOOP
     if [%1]==[] goto :EXECUTE
+    REM additionnal parsing : if %1 starts with -D, add it to SYSTEM props
+    set "PARSING=%1"
+    if "%PARSING:~0,2%" == "-D" goto :PARSE_SYSTEM_PROPS
     if "%1" == "cli" goto :EXECUTE_CLI
     if "%1" == "ui" goto :EXECUTE_UI
     if "%1" == "osgi" goto :EXECUTE_OSGI
@@ -117,6 +122,16 @@ rem # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     set exitCode=2
     goto END
 goto :EXECUTE
+
+:PARSE_SYSTEM_PROPS
+    set "OPTION=%1"
+    set "VALUE=%~2"
+    set "SYSTEM_PROPS=%SYSTEM_PROPS% %OPTION%=^"%VALUE%^""
+    REM echo %JAVA_OPTS%
+    shift
+    shift
+goto :RUN_LOOP
+    
 
 :EXECUTE_UI
     set PIPELINE2_WS_LOCALFS=true
@@ -195,6 +210,7 @@ goto :RUN_LOOP
         ) else (
             rem set PATHS=!PATHS! system\webservice
             set PATHS=!PATHS! system\webservice
+            set JAVA_OPTS=-Dcom.sun.management.jmxremote %JAVA_OPTS%
         )
     ) else ( rem for cli and ui modes, through the simple-api package provided in the assembly
         if not exist "%PIPELINE2_HOME%\system\simple-api" (
@@ -229,7 +245,7 @@ goto :RUN_LOOP
         if %ENABLE_OSGI% == true (
             set PATHS=!PATHS! system\osgi\gogo
         ) else (
-            call:warn Shell can only be enabled under OSGi
+            REM call:warn Shell can only be enabled under OSGi
         )
     )
     if %ENABLE_PERSISTENCE% == true (
@@ -340,8 +356,8 @@ goto :RUN_LOOP
             rem -Djava.endorsed.dirs="%JAVA_HOME%\jre\lib\endorsed;%JAVA_HOME%\lib\endorsed;%PIPELINE2_HOME%\lib\endorsed" ^
             rem -Djava.ext.dirs="%JAVA_HOME%\jre\lib\ext;%JAVA_HOME%\lib\ext;%PIPELINE2_HOME%\lib\ext" ^
     )
-    call:warn Starting java: %COMMAND%
-
+    
+    REM call:warn Starting java: %COMMAND%
     if %ENABLE_SHELL% == true (
         rem endlocal & (
         rem     set "PIPELINE2_HOME=%PIPELINE2_HOME%"
